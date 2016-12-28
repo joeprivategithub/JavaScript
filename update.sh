7c5afd727022bfac64b3945a30ab13867023339c
joe-pc <liuqs@ixdtech.com>
2016-12-28 14:14:14 +0800

    RSzlOQwRZOepnIJBQcXpEgg5brH77hylKmsKxBAOZeI= ## 12/28/2016 14:14:14

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-commit_time=`date +%Y-%m-%d-%H:%M`
+
+commit_time=`date +%m/%d/%Y\ %H:%M:%S`
 commit_content=`openssl rand -base64 32`
 git add .
 git commit -am "$commit_content ## $commit_time"
