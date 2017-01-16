7570a0974ac75942d8cc4d5a2ee159e100dd9609
joeprivategithub <xd_zabbix_alert@sina.com>
2017-01-16 11:30:10 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 commit_time=`date +%m/%d/%Y\ %H:%M:%S`
-commit_content=`openssl rand -base64 32`
+commit_content=`openssl rand -base64 33`
 git add .
 git commit -am "$commit_content ## $commit_time"
 git push -u origin master
